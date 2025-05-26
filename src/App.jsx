8958607c379a12ef40042c230387963c16316a62
Mahmud Hasan Sizan <mahmudhasan3801@gmail.cm>
2025-05-27 00:14:21 +0600

    fix the bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import { useLocation } from "react-router-dom";
 import AppBanner from "./components/AppBanner/AppBanner";
 import CoffeeCarts from "./components/CoffeeCarts/CoffeeCarts";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import WhereToBuy from "./components/WhereToBuy/WhereToBuy";
-import { useEffect } from "react";
 
 function App() {
 
-  const location = useLocation();
-
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const redirectUrl = params.get('redirect');
-
-    if (redirectUrl) {
-      // Optional: Validate the redirect URL
-      window.location.href = redirectUrl;
-    }
-  }, [location]);
+  
   return (
     <div className="overflow-x-hidden">
       <Hero />
